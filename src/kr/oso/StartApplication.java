d6176779bbbd6ca4ff8d75c343d48c8b6bd63508
Andrew Antonov <antonov.andrew.jr@gmail.com>
2020-05-10 03:27:20 +0500

    Проект с интерфейсом бех файлов

--- src/kr/oso/StartApplication.java
+++ src/kr/oso/StartApplication.java
@@ -1,12 +1,6 @@
 package kr.oso;
 class StartApplication {
     public static void main(String[] args) {
-//        Maker firstM = new Maker("FirstM");
-//        Maker secondM = new Maker("SecondM");
-//        Consumer firstC = new Consumer("FirstC");
-//        firstM.start();
-//        secondM.start();
-//        firstC.start();
         new MainWindow();
     }
 }
